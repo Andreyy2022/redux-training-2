486729d59cf1b01be01ccbb5f7b89d61a54da789
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2024-03-10 14:15:45 +0300

    2

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -11,7 +12,19 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
+
+import { createStore } from "redux";
+
+function playlist(state = []) {
+  return state;
+}
+
+const store = createStore(playlist);
+
+store.subscribe(() => {
+  console.log('subscribe', store.getState());
+});
+
+store.dispatch({type: 'ADD_TRACK', payload: 'Smell like spirit'});
